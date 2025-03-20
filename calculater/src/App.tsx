483ec158f5fe87b2ba9d66d9f16ff52741499f56
D.Senal Danushka Galagedara <senalgalagedara3@gmail.com>
2025-03-20 15:56:10 +0530

    senal

--- calculater/src/App.tsx
+++ calculater/src/App.tsx
@@ -1,20 +1,38 @@
-import { useState } from 'react'
+import react from 'react'
 import './App.css'
+import React from 'react';
 
 function Numbers(){
   return(
-    <>
-    
-    </>
+    <div className='grid'>
+      <div className='numbers'>1</div>
+      <div className='numbers'>2</div>
+      <div className='numbers'>3</div>
+      <div className='numbers'>4</div>
+      <div className='numbers'>5</div>
+      <div className='numbers'>6</div>
+      <div className='numbers'>7</div>
+      <div className='numbers'>8</div>
+      <div className='numbers'>9</div>
+    </div>
+  )
+}
+function ZeronDot()
+{
+  return(
+    <div>
+      <div></div>
+      <div></div>
+    </div>
   )
 }
 function App() {
-
   return (
     <>
-
+      <Numbers/>
     </>
-  )
+  );
 }
+
 
 export default App
