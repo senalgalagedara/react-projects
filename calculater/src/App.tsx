a3a58e33513ba3ada7c570df5e9f0d7a44c891c5
D.Senal Danushka Galagedara <senalgalagedara3@gmail.com>
2025-03-21 12:26:05 +0530

    senal

--- calculater/src/App.tsx
+++ calculater/src/App.tsx
@@ -1,20 +1,51 @@
-import react from 'react'
-import './App.css'
-import React from 'react';
+import './App.css';
+import React, { useState } from 'react';
+
+const handleClick = (value: string) => {
+//  setButtonValue(value); // Update state with the clicked button's value
+};
+
+//const handleCalculate() => {}
 
 function Numbers(){
   return(
-    <div className='grid'>
-      <div className='numbers'>1</div>
-      <div className='numbers'>2</div>
-      <div className='numbers'>3</div>
-      <div className='numbers'>4</div>
-      <div className='numbers'>5</div>
-      <div className='numbers'>6</div>
-      <div className='numbers'>7</div>
-      <div className='numbers'>8</div>
-      <div className='numbers'>9</div>
+    <>
+    <div className='wrapper'>
+      <div className="display">
+          <input
+          className='textbtn'
+          type="text"
+        //  value={inputValue}
+        // onChange={handleInputChange}
+        // onKeyPress={handleKeyPress}
+        />
+      </div>
+      <div className='grid'>
+        <button className='numbers' onClick={() => handleClick('1')}>1</button>
+        <button className='numbers' onClick={() => handleClick('2')}>2</button>
+        <button className='numbers' onClick={() => handleClick('3')}>3</button>
+        
+        <button className='symbole' onClick={() => handleClick('*')}>C</button>
+
+        <button className='numbers' onClick={() => handleClick('4')}>4</button>
+        <button className='numbers' onClick={() => handleClick('5')}>5</button>
+        <button className='numbers' onClick={() => handleClick('6')}>6</button>
+
+        <button className='symbole' onClick={() => handleClick('-')}>-</button>
+
+        <button className='numbers' onClick={() => handleClick('7')}>7</button>
+        <button className='numbers' onClick={() => handleClick('8')}>8</button>
+        <button className='numbers' onClick={() => handleClick('9')}>9</button>
+
+        <button className='symbole'>+</button>
+
+        <button className='numbers' onClick={() => handleClick('0')}>0</button>
+        <button className='symbole' onClick={() => handleClick('/')}>÷</button>
+        <button className='symbole' onClick={() => handleClick('%')}>×</button>
+        <button className='symbole' onClick={() => handleCalculate('=')}>=</button>      
+      </div>
     </div>
+    </>
   )
 }
 function ZeronDot()
